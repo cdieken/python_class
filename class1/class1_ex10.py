77791fa75ef205d2a049119c4899285c81833942
Applied Python user <cdieken@verisign.com>
2015-10-29 19:50:24 +0000

    corrected ex10 code

--- class1/class1_ex10.py
+++ class1/class1_ex10.py
@@ -1,5 +1,6 @@
 #!/home/cdieken/applied_python/bin/python
 
+import re
 from ciscoconfparse import CiscoConfParse
 
 
@@ -7,10 +8,21 @@ cisco_cfg = CiscoConfParse("cisco.txt")
 
 crypto_line = cisco_cfg.find_objects_wo_child(r"^crypto map CRYPTO", r"AES")
 
+'''
 for line in crypto_line:
     print 
     print line.text
     for child in line.children:
         print child.text
+print
+'''
+
+print "\nCrypto maps not using AES:"
+for line in crypto_line:
+    for child in line.children:
+        if 'transform' in child.text:
+            match = re.search(r"set transform-set (.*)$", child.text)
+            encryption = match.group(1)
+    print "  {0} >>> {1}".format(line.text.strip(), encryption)
 print
 
